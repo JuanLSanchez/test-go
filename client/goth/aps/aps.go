8c3470068d1eaef20faf36be99220dcd63c24e28
Juan L. Sánchez <juanlu-28a@hotmail.com>
2016-10-11 20:54:32 +0200

    feat: created the implementation

--- client/goth/aps/aps.go
+++ client/goth/aps/aps.go
@@ -1,6 +1,14 @@
 package aps
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
 	"github.com/markbates/goth"
 	"golang.org/x/oauth2"
 )
@@ -22,17 +30,82 @@ type Provider struct {
 
 // New - Please fill the code
 func New(clientKey, secret, callbackURL string, scopes ...string) *Provider {
-	return nil
+	provider := &Provider{
+		ClientKey:   clientKey,
+		Secret:      secret,
+		CallbackURL: callbackURL,
+	}
+
+	provider.config = &oauth2.Config{
+		ClientID:     provider.ClientKey,
+		ClientSecret: provider.Secret,
+		RedirectURL:  provider.CallbackURL,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  authURL,
+			TokenURL: tokenURL,
+		},
+		Scopes: []string{},
+	}
+
+	return provider
 }
 
 // FetchUser - Please fill the code
 func (p *Provider) FetchUser(session goth.Session) (goth.User, error) {
-	return goth.User{}, nil
+	sess := session.(*Session)
+	user := goth.User{
+		AccessToken:  sess.AccessToken,
+		Provider:     p.Name(),
+		RefreshToken: sess.RefreshToken,
+		ExpiresAt:    sess.ExpiresAt,
+	}
+
+	response, err := http.Get(endpointProfile + "?access_token=" + url.QueryEscape(sess.AccessToken))
+
+	if err == nil{
+		defer response.Body.Close()
+		bits, err := ioutil.ReadAll(response.Body)
+		if err == nil {
+			err = json.NewDecoder(bytes.NewReader(bits)).Decode(&user.RawData)
+			if err == nil {
+				err = userFromReader(bytes.NewReader(bits), &user)
+			}
+		}
+	}else {
+		if response != nil {
+			response.Body.Close()
+		}
+	}
+
+	return user, err
+}
+
+func userFromReader(reader io.Reader, user *goth.User) error {
+	u := struct {
+		ID        string `json:"id"`
+		Email     string `json:"email"`
+		Location   string `json:"location"`
+	}{}
+
+	err := json.NewDecoder(reader).Decode(&u)
+	if err == nil {
+		user.Email = u.Email
+		user.UserID = u.ID
+		user.Location = u.Location
+	}
+
+	return err
 }
 
 // RefreshToken - Please fill the code
 func (p *Provider) RefreshToken(refreshToken string) (*oauth2.Token, error) {
-	return nil, nil
+ 	token := &oauth2.Token{RefreshToken: refreshToken}
+	ts := p.config.TokenSource(oauth2.NoContext, token)
+	newToken, err := ts.Token()
+	if err != nil {
+		return nil, err
+	}
+	return newToken, err
 }
 
 // RefreshTokenAvailable - Please fill the code
@@ -50,9 +123,21 @@ func (p *Provider) Debug(debug bool) {}
 
 // BeginAuth - Please fill the code
 func (p *Provider) BeginAuth(state string) (goth.Session, error) {
-	return nil, nil
+	var opts []oauth2.AuthCodeOption
+	if p.prompt != nil {
+		opts = append(opts, p.prompt)
+	}
+	url := p.config.AuthCodeURL(state, opts...)
+	session := &Session{
+		AuthURL: url,
+	}
+	return session, nil
 }
 
 // SetPrompt - Please fill the code
 func (p *Provider) SetPrompt(prompt ...string) {
+	if len(prompt) == 0 {
+		return
+	}
+	p.prompt = oauth2.SetAuthURLParam("prompt", strings.Join(prompt, " "))
 }
